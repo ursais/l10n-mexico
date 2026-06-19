6af6ce34423b96546ec26c73283b9602669e6302
Roberto <roberto@cloudlotus.net>
2026-06-19 05:04:07 -0600

    [REM] l10n_mx_sat: Remove res_config_settings model and associated tests

--- l10n_mx_sat/models/__init__.py
+++ l10n_mx_sat/models/__init__.py
@@ -2,6 +2,5 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import res_company
-from . import res_config_settings
 from . import l10n_mx_sat_document
 from . import l10n_mx_sat_download_request
